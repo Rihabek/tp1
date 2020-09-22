f5779c4db086527fc03ed23231d9f5f3d3a3adc5
rihabek <rihabelkhadiri@hotmail.com>
2020-09-22 17:22:57 +0200

    exo5

--- tp1-2/fonctions_SDL.c
+++ tp1-2/fonctions_SDL.c
@@ -4,4 +4,8 @@
 
 
 
-SDL_Texture* charger_image (const char* nomfichier, SDL_Renderer*renderer);
+SDL_Texture* charger_image(const char* nomfichier, SDL_Renderer*renderer)
+{
+  nomfichier->fond = load_image( "fond.bmp",renderer);
+
+}
--- tp1-2/fonctions_SDL.h
+++ tp1-2/fonctions_SDL.h
@@ -4,31 +4,10 @@
 
 
 
+// struct textures{
+//     SDL_Texture* fond; /*!< Texture liée à l'image du fond de l'écran. */
+// };
 
 SDL_Texture* charger_image (const char* nomfichier, SDL_Renderer*renderer);
-//
-// // Créer un contexte de rendu (renderer) pour l’image
-// SDL_Renderer* SDL_CreateRenderer(SDL_Window* window, int index, Uint32 flags) ;
-// // Charger une image
-// SDL_Surface* SDL_LoadBMP(const char* file) ;
-// // Convertir la surface de l’image au format texture avant de l’appliquer
-// SDL_Texture* SDL_CreateTextureFromSurface(SDL_Renderer* renderer,
-// SDL_Surface* surface) ;
-// // Copier (une partie de) la texture dans le renderer
-// int SDL_RenderCopy(SDL_Renderer* renderer,
-// SDL_Texture* texture,
-// const SDL_Rect* srcrect,
-// const SDL_Rect* dstrect) ;
-// // Récupérer les attributs d’une texture
-// int SDL_QueryTexture(SDL_Texture* texture,
-// Uint32* format,
-// int* access,
-// int* w,
-// int* h) ;
-//
-// // Libérer une surface
-// void SDL_FreeSurface(SDL_Surface* surface) ;
-// // Libérer une texture
-// void SDL_DestroyTexture(SDL_Texture* texture) ;
-//
+
 #endif
